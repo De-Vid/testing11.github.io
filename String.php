92c1ff35f3beb5105a410453ef4be1fdaad33901
De-Vid <139108709+De-Vid@users.noreply.github.com>
2024-01-08 22:35:04 +0700

    Add files via upload

---
+++ String.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function Add(){
+            static $a = 0;
+            echo $a;
+            $a++;
+        }
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        echo "<br>";
+        Add();
+        print "<br>";
+
+        // ---------------- BOC  ----------------
+    ?>
+</body>
+</html>
